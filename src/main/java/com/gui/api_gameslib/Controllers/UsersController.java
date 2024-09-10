b2b6a554aaf7a91bc248c0ed271d76556e3a6cd8
gui2310g <fontesguilherme57@gmail.com>
2024-09-10 16:01:52 -0300

    refact userController with ResponseEntity

--- src/main/java/com/gui/api_gameslib/Controllers/UsersController.java
+++ src/main/java/com/gui/api_gameslib/Controllers/UsersController.java
@@ -4,6 +4,7 @@ import com.gui.api_gameslib.Models.Users;
 import com.gui.api_gameslib.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,28 +15,33 @@ public class UsersController {
     private UserService userService;
 
     @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Users CreateUser(@RequestBody Users users) {
-        return userService.CreateUser(users);
+    public ResponseEntity<Users> CreateUser(@RequestBody Users users) {
+        Users createdUser = userService.CreateUser(users);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     @GetMapping("/findAll")
-    public Iterable<Users> FindAllUsers() {
-        return userService.FindAllUsers();
+    public ResponseEntity<Iterable<Users>> FindAllUsers() {
+        Iterable<Users> users = userService.FindAllUsers();
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/find/{id}")
-    public Users FindUserById(@PathVariable Integer id) {
-        return userService.FindUserById(id);
+    public ResponseEntity<Users> FindUserById(@PathVariable Integer id) {
+        Users selectedUser = userService.FindUserById(id);
+        return ResponseEntity.ok(selectedUser);
     }
 
     @PutMapping("/update/{id}")
-    public Users updateUser(@RequestBody Users users, @PathVariable  Integer id) {
-        return userService.updateUser(users, id);
+    public ResponseEntity<Users> updateUser(@RequestBody Users users, @PathVariable  Integer id) {
+        Users updatedUser = userService.updateUser(users, id);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/delete/{id}")
-    public Users deleteUser(@PathVariable Integer id) {
-        return userService.DeleteUser(id);
+    public ResponseEntity<Users> deleteUser(@PathVariable Integer id) {
+        Users deletedUser = userService.DeleteUser(id);
+        return ResponseEntity.ok(deletedUser);
     }
 }
