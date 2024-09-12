2a26c87230e05cec0afbbaa45b0af1b0d07cd1f4
gui2310g <fontesguilherme57@gmail.com>
2024-09-12 20:17:54 -0300

    created genres repository

---
+++ src/main/java/com/gui/api_gameslib/Repositories/GenresRepository.java
@@ -0,0 +1,8 @@
+package com.gui.api_gameslib.Repositories;
+
+import com.gui.api_gameslib.Models.Genres;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenresRepository extends CrudRepository<Genres, Integer> {
+
+}
