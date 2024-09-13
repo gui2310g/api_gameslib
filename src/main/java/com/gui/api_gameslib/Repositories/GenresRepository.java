0f623d1fe622c78857147726bc2fc3bf33b7d3b9
gui2310g <fontesguilherme57@gmail.com>
2024-09-12 21:00:10 -0300

    added findAll in GenresRepository

--- src/main/java/com/gui/api_gameslib/Repositories/GenresRepository.java
+++ src/main/java/com/gui/api_gameslib/Repositories/GenresRepository.java
@@ -3,8 +3,11 @@ package com.gui.api_gameslib.Repositories;
 import com.gui.api_gameslib.Models.Genres;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GenresRepository extends CrudRepository<Genres, Integer> {
     Optional<Genres> findById(Integer id);
+
+    List<Genres> findAll();
 }
