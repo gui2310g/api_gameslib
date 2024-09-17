92af87a487222bc7bec5d29429664024b9107293
gui2310g <fontesguilherme57@gmail.com>
2024-09-16 23:35:47 -0300

    added addPublishers in Publishers Service

--- src/main/java/com/gui/api_gameslib/Services/PublishersService.java
+++ src/main/java/com/gui/api_gameslib/Services/PublishersService.java
@@ -1,6 +1,8 @@
 package com.gui.api_gameslib.Services;
 
+import com.gui.api_gameslib.Models.Publishers;
 import com.gui.api_gameslib.Repositories.PublishersRepository;
+import com.gui.api_gameslib.exceptions.GamesException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,11 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PublishersService {
     private final PublishersRepository publishersRepository;
+
+    public Publishers addPublishers(Publishers publishers) throws GamesException {
+        if(publishersRepository.findByName(publishers.getName()).isPresent())
+            throw new GamesException("This publisher is already added");
+
+        return publishersRepository.save(publishers);
+    }
 }
