000193e8567d993e9f7798ec9d085bac65828500
gui2310g <fontesguilherme57@gmail.com>
2024-09-16 21:49:12 -0300

    added findbyId in publishers repository

--- src/main/java/com/gui/api_gameslib/Repositories/PublishersRepository.java
+++ src/main/java/com/gui/api_gameslib/Repositories/PublishersRepository.java
@@ -3,5 +3,9 @@ package com.gui.api_gameslib.Repositories;
 import com.gui.api_gameslib.Models.Publishers;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PublishersRepository extends CrudRepository<Publishers, Integer> {
+
+    Optional<Publishers> findById(Integer id);
 }
