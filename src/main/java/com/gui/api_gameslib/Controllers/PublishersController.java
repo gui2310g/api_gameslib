f151abb6f410e389d6aa850a693a6f4bce1b72d4
gui2310g <fontesguilherme57@gmail.com>
2024-09-17 00:27:35 -0300

    added addPublishers in PublishersController

--- src/main/java/com/gui/api_gameslib/Controllers/PublishersController.java
+++ src/main/java/com/gui/api_gameslib/Controllers/PublishersController.java
@@ -1,7 +1,12 @@
 package com.gui.api_gameslib.Controllers;
 
+import com.gui.api_gameslib.Models.Publishers;
 import com.gui.api_gameslib.Services.PublishersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublishersController {
     @Autowired
     private PublishersService publishersService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Publishers> addPublishers(@RequestBody Publishers publishers) {
+        Publishers addedPublisher = publishersService.addPublishers(publishers);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedPublisher);
+    }
 }
