1268c919dfa4c93c7ee069e95c464b9d10a1c763
gui2310g <fontesguilherme57@gmail.com>
2024-09-17 00:31:24 -0300

    added findAllPublishers in Publishers Service

--- src/main/java/com/gui/api_gameslib/Controllers/PublishersController.java
+++ src/main/java/com/gui/api_gameslib/Controllers/PublishersController.java
@@ -5,10 +5,9 @@ import com.gui.api_gameslib.Services.PublishersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/publishers")
@@ -21,5 +20,12 @@ public class PublishersController {
         Publishers addedPublisher = publishersService.addPublishers(publishers);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(addedPublisher);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Publishers>> findAllPublishers() {
+        List<Publishers> publishers = publishersService.findAllPublishers();
+
+        return ResponseEntity.ok(publishers);
     }
 }
