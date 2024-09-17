9bc47b23f7a1ce9f14def07eec190b397c1fe102
gui2310g <fontesguilherme57@gmail.com>
2024-09-16 21:49:51 -0300

    added findAll in publishers repository

--- src/main/java/com/gui/api_gameslib/Repositories/PublishersRepository.java
+++ src/main/java/com/gui/api_gameslib/Repositories/PublishersRepository.java
@@ -3,9 +3,12 @@ package com.gui.api_gameslib.Repositories;
 import com.gui.api_gameslib.Models.Publishers;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PublishersRepository extends CrudRepository<Publishers, Integer> {
 
     Optional<Publishers> findById(Integer id);
+
+    List<Publishers> findAll();
 }
