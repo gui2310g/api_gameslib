491451b67ea50f2e3211473a83684e7c52f2da36
gui2310g <fontesguilherme57@gmail.com>
2024-10-27 20:03:22 -0300

    added findAllGenresByGameId in genres controller

--- src/main/java/com/gui/api_gameslib/controllers/GenresController.java
+++ src/main/java/com/gui/api_gameslib/controllers/GenresController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/genres")
 public class GenresController {
@@ -30,5 +32,11 @@ public class GenresController {
     public ResponseEntity<Genres> addGenresToGame(@PathVariable Integer genresId, @PathVariable Integer gameId) {
         Genres addedGenresToGame = genresService.addGenresToGame(genresId, gameId);
         return ResponseEntity.status(HttpStatus.CREATED).body(addedGenresToGame);
+    }
+
+    @GetMapping("/game/{gameId}")
+    public ResponseEntity<List<Genres>> findAllGenresByGameId(@PathVariable Integer gameId) {
+        List<Genres> genres = genresService.findAllGenresByGameId(gameId);
+        return ResponseEntity.ok(genres);
     }
 }
